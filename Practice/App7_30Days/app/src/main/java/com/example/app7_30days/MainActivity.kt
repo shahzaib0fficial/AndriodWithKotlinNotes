27fd0169b56a7613a5b50c60b90ab16850ba4a87
shahzaib0fficial <shahzaib5330884@gmail.com>
2024-05-07 23:09:07 +0500

    basic app is ready

--- Practice/App7_30Days/app/src/main/java/com/example/app7_30days/MainActivity.kt
+++ Practice/App7_30Days/app/src/main/java/com/example/app7_30days/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
 package com.example.app7_30days
 
 import android.os.Bundle
@@ -8,17 +10,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app7_30days.ui.theme.App7_30DaysTheme
 import androidx.compose.ui.unit.dp
+import com.example.app7_30days.data.DataSource
+import com.example.app7_30days.model.CookingClass
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CookingCard()
+                    CookingApp()
                 }
             }
         }
@@ -38,7 +48,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CookingCard(modifier: Modifier = Modifier) {
+fun CookingCard(cook: CookingClass,modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
             .padding(8.dp)
@@ -48,32 +58,63 @@ fun CookingCard(modifier: Modifier = Modifier) {
                 .padding(12.dp)
         ) {
             Text(
-                text = "Day1",
+                text = stringResource(id = cook.day),
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
-                text = "Here Recipe Name",
+                text = stringResource(id = cook.title),
                 style = MaterialTheme.typography.titleLarge
             )
             Image(
-                painter = painterResource(id = R.drawable.day1_dish_image),
+                painter = painterResource(id = cook.image),
                 contentDescription = "Here Recipe Name",
                 contentScale = ContentScale.Crop,
                 modifier = modifier
                     .fillMaxWidth()
             )
             Text(
-                text = "Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. Here Description of Recipe. ",
+                text = stringResource(id = cook.description),
                 style = MaterialTheme.typography.bodyLarge
             )
         }
     }
 }
 
+@Composable
+fun CookingList(cookingList: List<CookingClass>) {
+    Scaffold(
+        topBar = {
+            TopAppBar()
+        }
+    ) { it->
+        LazyColumn(contentPadding = it) {
+            items(cookingList) {
+                CookingCard(cook = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun TopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "Cooking App"
+            )
+        }
+    )
+}
+
+@Composable
+fun CookingApp() {
+    CookingList(cookingList = DataSource().listOfCooking())
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CookingPreview() {
     App7_30DaysTheme {
-        CookingCard()
+        CookingApp()
     }
 }
--- Practice/App7_30Days/app/src/main/java/com/example/app7_30days/data/DataSource.kt
+++ Practice/App7_30Days/app/src/main/java/com/example/app7_30days/data/DataSource.kt
@@ -1,4 +1,41 @@
 package com.example.app7_30days.data
 
+import com.example.app7_30days.R
+import com.example.app7_30days.model.CookingClass
+
 class DataSource {
+    fun listOfCooking(): List<CookingClass> {
+        return listOf(
+            CookingClass(R.string.day1,R.string.day1_dish_title,R.drawable.day1_dish_image,R.string.day1_description),
+            CookingClass(R.string.day2,R.string.day2_dish_title,R.drawable.day2_dish_image,R.string.day2_description),
+            CookingClass(R.string.day3,R.string.day3_dish_title,R.drawable.day3_dish_image,R.string.day3_description),
+            CookingClass(R.string.day4,R.string.day4_dish_title,R.drawable.day4_dish_image,R.string.day4_description),
+            CookingClass(R.string.day5,R.string.day5_dish_title,R.drawable.day5_dish_image,R.string.day5_description),
+            CookingClass(R.string.day6,R.string.day6_dish_title,R.drawable.day6_dish_image,R.string.day6_description),
+            CookingClass(R.string.day7,R.string.day7_dish_title,R.drawable.day7_dish_image,R.string.day7_description),
+            CookingClass(R.string.day8,R.string.day8_dish_title,R.drawable.day8_dish_image,R.string.day8_description),
+            CookingClass(R.string.day9,R.string.day9_dish_title,R.drawable.day9_dish_image,R.string.day9_description),
+            CookingClass(R.string.day10,R.string.day10_dish_title,R.drawable.day10_dish_image,R.string.day10_description),
+            CookingClass(R.string.day11,R.string.day11_dish_title,R.drawable.day11_dish_image,R.string.day11_description),
+            CookingClass(R.string.day12,R.string.day12_dish_title,R.drawable.day12_dish_image,R.string.day12_description),
+            CookingClass(R.string.day13,R.string.day13_dish_title,R.drawable.day13_dish_image,R.string.day13_description),
+            CookingClass(R.string.day14,R.string.day14_dish_title,R.drawable.day14_dish_image,R.string.day14_description),
+            CookingClass(R.string.day15,R.string.day15_dish_title,R.drawable.day15_dish_image,R.string.day15_description),
+            CookingClass(R.string.day16,R.string.day16_dish_title,R.drawable.day16_dish_image,R.string.day16_description),
+            CookingClass(R.string.day17,R.string.day17_dish_title,R.drawable.day17_dish_image,R.string.day17_description),
+            CookingClass(R.string.day18,R.string.day18_dish_title,R.drawable.day18_dish_image,R.string.day18_description),
+            CookingClass(R.string.day19,R.string.day19_dish_title,R.drawable.day19_dish_image,R.string.day19_description),
+            CookingClass(R.string.day20,R.string.day20_dish_title,R.drawable.day20_dish_image,R.string.day20_description),
+            CookingClass(R.string.day21,R.string.day21_dish_title,R.drawable.day21_dish_image,R.string.day21_description),
+            CookingClass(R.string.day22,R.string.day22_dish_title,R.drawable.day22_dish_image,R.string.day22_description),
+            CookingClass(R.string.day23,R.string.day23_dish_title,R.drawable.day23_dish_image,R.string.day23_description),
+            CookingClass(R.string.day24,R.string.day24_dish_title,R.drawable.day24_dish_image,R.string.day24_description),
+            CookingClass(R.string.day25,R.string.day25_dish_title,R.drawable.day25_dish_image,R.string.day25_description),
+            CookingClass(R.string.day26,R.string.day26_dish_title,R.drawable.day26_dish_image,R.string.day26_description),
+            CookingClass(R.string.day27,R.string.day27_dish_title,R.drawable.day27_dish_image,R.string.day27_description),
+            CookingClass(R.string.day28,R.string.day28_dish_title,R.drawable.day28_dish_image,R.string.day28_description),
+            CookingClass(R.string.day29,R.string.day29_dish_title,R.drawable.day29_dish_image,R.string.day29_description),
+            CookingClass(R.string.day30,R.string.day30_dish_title,R.drawable.day30_dish_image,R.string.day30_description),
+        )
+    }
 }
